91efceafd0393850d80ed339984f81557cc64534
Surajit Singha Sisir <sci.surajit@gmail.com>
2025-02-12 14:00:00 +0600

    Working for Color Palette
    Signed-off-by: Surajit Singha Sisir <sci.surajit@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,8 @@
     <main>
         <h1 style="text-align: center;">VISUAL WEAVER</h1>
         <!-- VISUAL WEAVER EDITOR -->
-        <textarea name="visual-weaver" class="hide" id="visual-weaver" aria-label="Visual Weaver">
-
-
-    </textarea>
+        <!-- <textarea name="visual-weaver" class="hide" id="visual-weaver" aria-label="Visual Weaver">
+    </textarea> -->
 
         <aside class="visual-weaver">
 
@@ -32,7 +30,7 @@
                         class="vwbtn vwbtn-default bold" id="bold" tabindex="-1" aria-label="Bold (CTRL+B)"
                         data-original-title="Bold (CTRL+B)" @click="bold"><i class="m-bold"></i></button>
                     <!-- ITALIC -->
-                    <button :class="{ 'pressed': editor?.isActive('bold') }" type="button"
+                    <button :class="{ 'pressed': editor?.isActive('italic') }" type="button"
                         class="vwbtn vwbtn-default italic " id="italic" tabindex="-1" aria-label="Italic (CTRL+I)"
                         data-original-title="Italic (CTRL+I)" @click="italic"><i class="m-italic"></i></button>
                     <!-- UNDERLINE -->
@@ -65,7 +63,6 @@
                         aria-label="Erase Styles (CTRL+/)" data-original-title="Erase Styles (CTRL+/)"
                         @click="eraseStyle"><i class="m-eraser"></i></button>
                 </div>
-
 
                 <!-- TEXT SIZER & FAMILY -->
                 <div class="text-sizer-family toolbar">
@@ -151,7 +148,36 @@
                             class="m-align-justify" style="font-size: 1.2rem;"></i></button>
                 </div>
 
+                <!-- TEXT COLORS -->
+                <div class="text-colors toolbar">
+                    <!-- COLOR PALETTE -->
+                    <!-- FONT COLOR -->
+                    <button :class="{ 'pressed': editor?.isActive('color') }" type="button"
+                        class="vwbtn vwbtn-default fontColor" id="fontColor" tabindex="-1" aria-label="Font Color"
+                        data-original-title="Font Color" @click="fontColor"><i class="m-text-color"></i></button>
+                    <!-- FONT COLOR -->
+                    <button :class="{ 'pressed': editor?.isActive('color') }" type="button"
+                        class="vwbtn vwbtn-default backgroundColor" id="backgroundColor" tabindex="-1"
+                        aria-label="Background Color" data-original-title="Background Color" @click="backgroundColor"><i
+                            class="m-palette"></i></button>
 
+
+                    <div class="colorModal hidei" ref="colorModal">
+                        <button class="w-100 m-b--05" ref="colorBanner"></button>
+                        <button v-for="color in colors" class="color-box btn"
+                            :style="`background-color: ${color.code} ;`" :aria-label="color.name"
+                            :data-original-title="color.name"></button>
+                    </div>
+
+                </div>
+
+
+                <!-- BACKGROUND COLOR PALETTE -->
+                <!-- <div class="background-color-palette">
+                        <button v-for="color in bgColors" :key="color" :style="{ backgroundColor: color }"
+                            @click="setBackgroundColor(color)" class="color-btn"
+                            :aria-label="'Background Color: ' + color"></button>
+                    </div> -->
             </div>
             <editor-content :editor="editor" class="editor editor-body editor-section" />
         </aside>
@@ -194,6 +220,37 @@ body {
     background-color: transparent;
     outline: none;
 }
+
+
+.colorModal {
+    position: absolute;
+    top: 120%;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 0.2rem;
+    padding: 0.5rem;
+    width: 10rem;
+    background: #f4f4f4;
+    border: 1px solid $border;
+    z-index: 999;
+}
+
+.color-box {
+    position: relative;
+    width: 1rem;
+    aspect-ratio: 1;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 12px;
+    font-weight: bold;
+    text-align: center;
+    box-shadow: 2px 2px 3px #00000033;
+}
 </style>
 
 
@@ -208,6 +265,7 @@ import FontFamily from '@tiptap/extension-font-family';
 import TextAlign from '@tiptap/extension-text-align';
 import Superscript from '@tiptap/extension-superscript';
 import Subscript from '@tiptap/extension-subscript';
+import { Color } from '@tiptap/extension-color'
 
 const editor = useEditor({
     content: '<p>Hello, start editing...</p>',
@@ -222,9 +280,120 @@ const editor = useEditor({
         }),
         Superscript,
         Subscript,
+        Color,
     ],
 });
 
+
+// List of colors for font and background
+const colors =
+    [
+        { "code": "#FFFFFF", "name": "White" },
+        { "code": "#000000", "name": "Black" },
+        { "code": "#FF0000", "name": "Red" },
+        { "code": "#00FF00", "name": "Green" },
+        { "code": "#0000FF", "name": "Blue" },
+        { "code": "#FFFF00", "name": "Yellow" },
+        { "code": "#00FFFF", "name": "Cyan" },
+        { "code": "#FF00FF", "name": "Magenta" },
+        { "code": "#00000000", "name": "Transparent" },
+
+        { "code": "#800000", "name": "Maroon" },
+        { "code": "#808000", "name": "Olive" },
+        { "code": "#008080", "name": "Teal" },
+        { "code": "#800080", "name": "Purple" },
+        { "code": "#808080", "name": "Gray" },
+        { "code": "#C0C0C0", "name": "Silver" },
+
+        { "code": "#424242", "name": "Tundora" },
+        { "code": "#636363", "name": "Dove Gray" },
+        { "code": "#9C9C94", "name": "Star Dust" },
+        { "code": "#CEC6CE", "name": "Pale Slate" },
+        { "code": "#EFEFEF", "name": "Gallery" },
+        { "code": "#F7F7F7", "name": "Alabaster" },
+        { "code": "#D3D3D3", "name": "Light Gray" },
+        { "code": "#A9A9A9", "name": "Dark Gray" },
+
+        { "code": "#E79C9C", "name": "Tonys Pink" },
+        { "code": "#E76363", "name": "Mandy" },
+        { "code": "#CE0000", "name": "Guardsman Red" },
+        { "code": "#9C0000", "name": "Sangria" },
+        { "code": "#630000", "name": "Rosewood" },
+        { "code": "#F08080", "name": "Light Coral" },
+        { "code": "#FA8072", "name": "Light Salmon" },
+        { "code": "#FFB6C1", "name": "Light Pink" },
+        { "code": "#FFC0CB", "name": "Pink" },
+        { "code": "#FF69B4", "name": "Hot Pink" },
+        { "code": "#FF1493", "name": "Deep Pink" },
+        { "code": "#C71585", "name": "Medium Violet Red" },
+        { "code": "#DB7093", "name": "Pale Violet Red" },
+        { "code": "#FF7F50", "name": "Coral" },
+        { "code": "#FF4500", "name": "Orange Red" },
+        { "code": "#FF6347", "name": "Tomato" },
+        { "code": "#FFA07A", "name": "Light Salmon" },
+        { "code": "#E9967A", "name": "Dark Salmon" },
+        { "code": "#DC143C", "name": "Crimson" },
+
+        { "code": "#FF9C00", "name": "Orange Peel" },
+        { "code": "#FFC69C", "name": "Peach Orange" },
+        { "code": "#FFE79C", "name": "Cream Brulee" },
+        { "code": "#F7AD6B", "name": "Rajah" },
+        { "code": "#FFD663", "name": "Dandelion" },
+        { "code": "#E79439", "name": "Fire Bush" },
+        { "code": "#EFC631", "name": "Golden Dream" },
+        { "code": "#B56308", "name": "Mai Tai" },
+        { "code": "#BD9400", "name": "Buddha Gold" },
+        { "code": "#FFD700", "name": "Gold" },
+        { "code": "#FFA500", "name": "Orange" },
+
+        { "code": "#ADFF2F", "name": "Green Yellow" },
+        { "code": "#7FFF00", "name": "Chartreuse" },
+        { "code": "#76EE00", "name": "Lawn Green" },
+        { "code": "#00FA9A", "name": "Medium Spring Green" },
+        { "code": "#00FF7F", "name": "Spring Green" },
+        { "code": "#006400", "name": "Dark Green" },
+        { "code": "#98FB98", "name": "Pale Green" },
+        { "code": "#8FBC8F", "name": "Dark Sea Green" },
+        { "code": "#3CB371", "name": "Medium Sea Green" },
+        { "code": "#20B2AA", "name": "Light Sea Green" },
+        { "code": "#90EE90", "name": "Light Green" },
+
+        { "code": "#40E0D0", "name": "Turquoise" },
+        { "code": "#48D1CC", "name": "Medium Turquoise" },
+        { "code": "#AFEEEE", "name": "Pale Turquoise" },
+        { "code": "#E0FFFF", "name": "Light Cyan" },
+        { "code": "#ADD8E6", "name": "Light Blue" },
+        { "code": "#87CEEB", "name": "Sky Blue" },
+        { "code": "#87CEFA", "name": "Light Sky Blue" },
+        { "code": "#00BFFF", "name": "Deep Sky Blue" },
+        { "code": "#1E90FF", "name": "Dodger Blue" },
+        { "code": "#4682B4", "name": "Steel Blue" },
+        { "code": "#5F9EA0", "name": "Cadet Blue" }
+    ];
+
+const colorModal = ref<HTMLElement | null>(null);
+const fontColor = () => {
+    colorModal.value?.classList.toggle("hidei");
+}
+const backgroundColor = () => {
+    colorModal.value?.classList.toggle("hidei");
+}
+
+
+
+// Set font color
+const setFontColor = (color: string) => {
+    if (editor.value) {
+        editor.value.chain().focus().setColor(color).run();
+    }
+};
+
+// Set background color
+const setBackgroundColor = (color: string) => {
+    if (editor.value) {
+        // editor.value.chain().focus().setBackgroundColor(color).run();
+    }
+};
 
 
 const alignLeft = () => {
@@ -251,8 +420,9 @@ const alignJustify = () => {
 
 
 // TEXT RESIZER AND FAMILY
-const selectedFontSize = ref("default"); // or any valid default size
-const selectedFontFamily = ref("default"); // Default font family
+const selectedFontSize = ref("default");
+const selectedFontFamily = ref("default");
+
 // SET FONT FAMILY
 const changeFontFamily = () => {
     if (editor.value) {
@@ -357,10 +527,7 @@ onMounted(() => {
     tooltips();
     resizer();
 
-
 });
-
-
 
 
 const toolbar = {
@@ -398,7 +565,7 @@ function tooltips(): void {
         button.addEventListener("mouseleave", () => {
             if (tooltip) {
                 tooltip.classList.remove("show");
-                setTimeout(() => tooltip?.remove(), 200);
+                setTimeout(() => tooltip?.remove(), 200);  // Ensure tooltip removal after fade-out
             }
         });
     });
